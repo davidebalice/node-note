7349be7e474d1d2859928bc2adcc0ce50130a9ec
Simona Tocci <simona@MBP-di-Simona.station>
2017-11-13 01:37:57 +0100

    error fixed

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 if(process.env.NODE_ENV === 'production'){
-    module.exports = {mongoURI: 'mongodb://<simo>:<simo>@ds159845.mlab.com:59845/note-prod'}
+    module.exports = {mongoURI: 'mongodb://simo:simo@ds159845.mlab.com:59845/note-prod'}
 }else{
     module.exports = {mongoURI: 'mongodb://localhost/note'}
 }
